7ab544284fa2829c0205057ab1f68b526b8f4a3d
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2025-05-13 16:28:34 +0200

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {CreateGamePageComponent} from './create-game-page/create-game-page.component';
+import {GameToJoinListPageComponent} from './game-to-join-list-page/game-to-join-list-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path:'create-game', component: CreateGamePageComponent},
+  {path:'list-games-to-join', component: GameToJoinListPageComponent},
+
+];
